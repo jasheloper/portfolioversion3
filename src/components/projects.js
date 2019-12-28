3695908b4387b801bf514a7ece5d3fc7c970a6ee
Jashele Tillman <jtilly@Jasheles-Air.domain>
2019-12-28 01:11:48 -0700

    modified projects section and tested responsiveness

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import pancakes from "../images/pancakes.jpg";
 import recipe from "../images/recipegen.png";
 import tvguide from '../images/tvguide.png';
+import portfolio from '../images/portfoliosite.png'
 
 function Projects() {
   return (
@@ -13,45 +14,57 @@ function Projects() {
           <div className="single-project">
             <div className="project-info">
             <h2>Portfolio Website</h2>
-              <h3>Technologies: React, LESS</h3>
-              <img src={pancakes} alt="Project" />
+              <h3>Tools: React, LESS</h3>
+              <img src={portfolio} alt="Project" />
             </div>
 
             <div className="project-info">
             
-             Portfolio site built solely for the purpose of displaying my work in web development, writing, and content creation.  A tool that is utilized for networking with others in the field including employers and a way to share my knowledge.
+             Portfolio site built solely for the purpose of displaying my work in web development, writing, and content creation.  A tool that is utilized for professional networking with others in the software (or similiar) fields including employers & a centralized location to share my knowledge and what I'm learning.
               <br />
-              <h4>Demo | GitHub</h4>
+              <h4>
+                
+                <a href="https://jashele.dev/" target="_blank">View Demo</a> -  <a href="https://github.com/jasheloper/portfolioversion3" target="_blank">GitHub Code</a>
+
+              </h4>
             </div>
           </div>
 
           <div className="single-project">
             <div className="project-info">
             <h2>Recipe Generator</h2>
-              <h3>Technologies: React, Axios, Redux</h3>
+              <h3>Tools: React, Axios, Redux</h3>
               <img src={recipe} alt="Project" />
             </div>
 
             <div className="project-info">
              
-              This React app allows users to generate a random meal on button click.  3rd party API data was implemented using Axios and the chosen data that users get on each request is: <code>meal photo, instructions, category,</code> and <code>area</code>.
+              This React app allows users to generate a random meal on button click.  3rd party API data was implemented using Axios and the selected data that users see on each request is: <code>title, photo, instructions, category,</code> and <code>area</code>.
 
-              <h4>Demo | GitHub</h4>
+              <h4>
+                
+                <a href="https://what-should-i-eat.netlify.com/" target="_blank">View Demo</a> -  <a href="https://github.com/jasheloper/React-Redux-App/tree/jashele-tillman" target="_blank">GitHub Code</a>
+
+              </h4>
             </div>
           </div>
 
           <div className="single-project">
             <div className="project-info">
             <h2>TV Guide</h2>
-              <h3>Technologies: React, React Hooks, AJAX</h3>
+              <h3>Tools: React, React Hooks, AJAX</h3>
               <img src={tvguide} alt="Project" />
             </div>
 
             <div className="project-info">
            
-              This React app allows users to see the latest episodes on television.  The data is generated from a live 3rd party API and is automatically updated each day.  Each request gets <code>tv image, overview, country, language,</code> and <code>vote average</code>.
+              This React app allows users to see the latest episodes on television.  The data is generated from a live 3rd party API and is automatically updated each day.  Each request gets <code>image, overview, country, language,</code> and <code>vote average</code>.
 
-              <h4>Demo | GitHub</h4>
+              <h4>
+                
+                <a href="https://jasheletv.netlify.com/" target="_blank">View Demo</a> -  <a href="https://github.com/jasheloper/jashele-tv" target="_blank">GitHub Code</a>
+
+              </h4>
             </div>
           </div>
         </div>
