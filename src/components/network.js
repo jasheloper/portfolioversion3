042d189490863f9b1f75884a255ed9dd905b0017
Jashele Tillman <jtilly@Jasheles-Air.domain>
2019-12-28 16:26:29 -0700

    more network tweaks

--- src/components/network.js
+++ src/components/network.js
@@ -18,7 +18,7 @@ function Network() {
 
                 <div className="network-item">
 
-                      <h1>Reach Out</h1>
+                      <h2>Reach Out</h2>
                       I am interested in learning about serious Full-Time opportunities and roles in Front End and Full Stack Web Development.  Here's a list of ways to network and connect: 
              <ul>
                     <li><img src ={linkedin} className="contact-icon"  alt="LinkedIn" /> <br /> Connect on <a target="_blank" rel="noopener noreferrer" href="http://example.com">LinkedIn</a></li>
@@ -38,7 +38,7 @@ function Network() {
                 </div>
 
                 <div className="network-item">
-                <h1>Media & Content</h1>
+                <h2>Media & Content</h2>
                 My work extends in the community by way of blogging and video content creation.  Every chance I get, I use my free time to post articles and show people what I'm working on and share knowledge. 
                 </div>
 
