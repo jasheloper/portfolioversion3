8aa02d5d2112debbbd3dd9599b6045f541687f9c
Jashele Tillman <jtilly@Jasheles-Air.domain>
2019-12-28 20:10:32 -0700

    nav links and back to top

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,13 @@ function Footer() {
           <h1>© 2019 - 2020</h1>
 
           <h2>
-          Jashele.dev was fully built, designed, and coded from the groundup by Jashele Tillman.
+          Jashele.Dev was fully built, designed, and coded from the ground up by   <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/jasheloper/portfolioversion3/"
+                >
+                 Jashele Tillman
+                </a>.
           </h2>
 
           <h3>
