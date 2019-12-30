239d866e5911df34b8466e73886040fb2f83cfce
Jashele Tillman <jftjashele@gmail.com>
2019-12-29 17:54:22 -0700

    scrollbar

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,25 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <h1>© 2019 - 2020</h1>
-
-      <h2>
-        Jashele.Dev was fully built, designed, and coded from the ground up by{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/jasheloper/portfolioversion3/"
-        >
-          Jashele Tillman
-        </a>
-        .
-      </h2>
-
-      <h3>
-        <a rel="noopener noreferrer" href="#nav">
-          Back To Top
-        </a>
-      </h3>
+      © 2019 - 2020
+      <br />
+      Built, designed, and coded by{" "}
+      <a rel="noopener noreferrer" href="#nav">
+        Jashele Tillman
+      </a>
+      .
     </footer>
   );
 }
